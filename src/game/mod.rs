4f813810edaf10e0508d8e395854177d9b4134e1
SamokhinIlya <samokhin.ilya.97@gmail.com>
2022-07-27 19:52:59 +0300

    add struct Canvas for convenient bitmap drawing

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,24 +1,11 @@
-pub fn update(raw_canvas: &mut impl RawCanvas, input: &Input) {
-    let width = raw_canvas.width();
-    let height = raw_canvas.height();
+use std::ops::IndexMut;
+use std::convert::TryInto;
 
-    for y in 0..height {
-        for x in 0..width {
-            let pixel = &mut raw_canvas[(y*width + x) as usize];
-            *pixel = (x as u32) << 8 | (y as u32);
-        }
-    }
-
-    let mouse = &input.mouse;
-    for y in mouse.y..(mouse.y + 4) {
-        for x in mouse.x..(mouse.x + 4) {
-            let pixel = &mut raw_canvas[(y as usize)*width + (x as usize)];
-            *pixel = 0x00FFFFFF;
-        }
-    }
+pub fn update(raw_canvas: &mut dyn RawCanvas, input: &Input) {
+    update_impl(Canvas::from_raw(raw_canvas), input)
 }
 
-pub trait RawCanvas: std::ops::IndexMut<usize, Output=u32> {
+pub trait RawCanvas: IndexMut<usize, Output=u32> {
     fn width(&self) -> usize;
     fn height(&self) -> usize;
 }
@@ -34,4 +21,52 @@ pub struct Mouse {
     pub y: i32,
     pub left: bool,
     pub right: bool,
+}
+
+fn update_impl(mut c: Canvas, input: &Input) {
+    for y in 0..c.height() {
+        for x in 0..c.width() {
+            c.get_mut(x, y).map(|p| *p = (x as u32) << 8 | (y as u32));
+        }
+    }
+
+    let mouse = &input.mouse;
+    for y in mouse.y..(mouse.y + 4) {
+        for x in mouse.x..(mouse.x + 4) {
+            c.get_mut(x, y).map(|p| *p = 0x00FFFFFF);
+        }
+    }
+}
+
+struct Canvas<'a> {
+    inner: &'a mut dyn RawCanvas,
+}
+
+impl<'a> Canvas<'a> {
+    pub fn from_raw(raw_canvas: &'a mut dyn RawCanvas) -> Self {
+        let _: i32 = raw_canvas.width().try_into().expect("failed to convert RawCanvas::width() to i32");
+        let _: i32 = raw_canvas.height().try_into().expect("failed to convert RawCanvas::height() to i32");
+        Self {
+            inner: raw_canvas,
+        }
+    }
+
+    pub fn width(&self) -> i32 {
+        self.inner.width().try_into().unwrap()
+    }
+
+    pub fn height(&self) -> i32 {
+        self.inner.height().try_into().unwrap()
+    }
+
+    pub fn get_mut(&mut self, x: i32, y: i32) -> Option<&mut u32> {
+        let width = self.width();
+        let height = self.height();
+
+        if x > 0 && x < width && y > 0 && y < height {
+            Some(&mut self.inner[(y*width + x).try_into().unwrap()])
+        } else {
+            None
+        }
+    }
 }
