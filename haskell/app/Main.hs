0034a03ff93f5e48f32b231da76fa2233ed6669a
SamokhinIlya <samokhin.ilya.97@gmail.com>
2022-12-21 18:43:17 +0300

    replace Win32 -> gloss Win32 doesn't have neither StretchDIBits nor BitBlt

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,104 +1,24 @@
 module Main where
 
-import Msg
-
-import Foreign.Storable
-import Foreign.Ptr
-
-import System.Win32.DLL (getModuleHandle)
-import System.Win32.Types (HINSTANCE)
-
-import qualified Graphics.Win32 (createWindowEx)
-import Graphics.Win32 ( mkClassName
-                      , registerClass
-                      , showWindow
-                      , getDC
-                      , c_PeekMessage
-                      , allocaMessage
-                      , translateMessage
-                      , dispatchMessage
-                      , postQuitMessage
-                      )
-import Graphics.Win32 ( wS_OVERLAPPEDWINDOW
-                      , cS_HREDRAW, cS_VREDRAW
-                      , LPPAINTSTRUCT
-                      , RECT
-                      , HDC
-                      , WindowMessage
-                      , LRESULT
-                      , defWindowProcSafe
-                      , sW_SHOWNORMAL
-                      , HWND
-                      , UINT
-                      , WPARAM
-                      , LPARAM
-                      , DWORD
-                      , POINT
-                      )
-import Graphics.Win32.Window (ClassName)
-
-import Graphics.Win32.Message ( wM_QUIT
-                              , wM_DESTROY
-                              --, wM_SETCURSOR - no SetCursor function
-                              --, wM_EXITSIZEMOVE - no message
-                              )
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Interact
 
 main :: IO ()
-main = do
-    currentInstance <- getModuleHandle Nothing
-    className <- createClass currentInstance "className"
-    window <- createWindow currentInstance className "Window title" 1280 720
-    showWindow window sW_SHOWNORMAL
-    deviceContext <- getDC (Just window)
-    let mainLoop = do
-            continue <- messagePump
-            if continue
-                then mainLoop
-                else return ()
-        in mainLoop
+main = play (InWindow "title" (1280, 720) (100, 100)) black updatesPerSecond mkData render handleEvent update
 
-messagePump :: IO Bool
-messagePump = allocaMessage $ \ptrMsg ->
-    let handleMsg = do
-            res <- c_PeekMessage ptrMsg nullPtr 0 0 1
-            if res > 0
-                then do
-                    msg <- peek (castPtr ptrMsg :: Ptr MSG)
-                    let m = message msg
-                    if m == wM_QUIT
-                        then return False
-                        else do
-                            () <$ translateMessage ptrMsg
-                            () <$ dispatchMessage ptrMsg
-                            handleMsg
-                else return True
-    in handleMsg
+updatesPerSecond :: Int
+updatesPerSecond = 60
 
-createWindow :: HINSTANCE -> ClassName -> String -> Int -> Int -> IO HWND
-createWindow inst className title width height =
-    let exStyle = 0
-        style = wS_OVERLAPPEDWINDOW
-        x = Nothing
-        y = Nothing
-        w = Just width
-        h = Just height
-        parent = Nothing
-        menu = Nothing
-    in Graphics.Win32.createWindowEx exStyle className title style x y w h parent menu inst wndProc
+data Data = Data
 
-createClass :: HINSTANCE -> String -> IO ClassName
-createClass currentInstance name = do
-    let style = cS_HREDRAW + cS_VREDRAW
-        icon = Nothing
-        cursor = Nothing
-        brush = Nothing
-        menu = Nothing
-        className = mkClassName name
-    registerClass (style, currentInstance, icon, cursor, brush, menu, className)
-    return className
+mkData :: Data
+mkData = Data
 
-wndProc :: HWND -> WindowMessage -> WPARAM -> LPARAM -> IO LRESULT
-wndProc hwnd wmsg wParam lParam
-    | wmsg == wM_DESTROY   = do postQuitMessage 0
-                                return 0
-    | otherwise            = defWindowProcSafe (Just hwnd) wmsg wParam lParam
+render :: Data -> Picture
+render = const blank
+
+handleEvent :: Event -> Data -> Data
+handleEvent event d = d
+
+update :: Float -> Data -> Data
+update dt d = d
