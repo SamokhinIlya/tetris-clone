1ccd435dceea3f79983cc774e003b3fc407d8846
SamokhinIlya <samokhin.ilya.97@gmail.com>
2022-12-22 21:16:06 +0300

    Grid -> Field implement width Data.Array

--- haskell/app/Game.hs
+++ haskell/app/Game.hs
@@ -16,34 +16,21 @@ mkData
     = Data
     { field = fill4BottomRows mkField
     }
-    where
-        fill4BottomRows :: Field -> Field
-        fill4BottomRows g = g { gridValues = top ++ map (map (const Falling)) bottom }
-            where (top, bottom) = splitAt (gridHeight g - 4) (gridValues g)
+    where fill4BottomRows :: Field -> Field
+          fill4BottomRows f = f // [((y, x), Falling) | y <- [y0 .. y1]
+                                                      , x <- [x0 .. x1] ]
+            where ((y0, x0), (y1, x1)) = bounds f
 
 update :: Data -> Canvas -> Input -> Float -> (Data, Canvas)
 update d canvas input dt = (d, Draw.cell white (100, 100) 30 canvas)
 
-data Grid a
-    = Grid
-    { gridValues :: [[a]]
-    , gridWidth :: Int
-    , gridHeight :: Int
-    }
-
-mkGrid :: Int -> Int -> a -> Grid a
-mkGrid width height value
-    = Grid
-    { gridValues = values
-    , gridWidth = width
-    , gridHeight = height
-    }
-    where values = replicate height row
-          row    = replicate width value
-
 data Cell = Empty | Falling | Frozen | Disappearing
 
-type Field = Grid Cell
+type Field = Array (Int, Int) Cell
 
 mkField :: Field
-mkField = mkGrid 10 20 Empty
+mkField = array ((y0, x0), (y1, x1)) [((y, x), Empty) | y <- [y0 .. y1]
+                                                      , x <- [x0 .. x1] ]
+    where (y0, y1)        = (0 , height - 1)
+          (x0, x1)        = (0 , width - 1 )
+          (height, width) = (20, 10        )
